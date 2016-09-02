7215bd344fb5fc7f29e0f8e2b14ee01fb1deacce
rhys13th <gulati21@live.com>
2016-09-01 19:55:59 -0700

    Updated comments, created functions

---
+++ src/Driver.java
@@ -0,0 +1,32 @@
+//@author: Kirtwinder Gulati
+
+/**
+ *   Given the number of cannonballs on each side of the base, calculate the total number of cannonballs in the entire
+ *   tetrahedral stack
+ *
+ *   Input: The first line contains "n", giving the number of tetrahedral problems posed, for a maximum of 100 problems.
+ *      Following that are exactly "n" lines, each with a single number giving the number of cannonballs on each side
+ *      of the base, a positive integer from 0 to 100, not inclusive.
+ *   Output: The problem number, followed by a colon(:), a blank space, the number of cannonballs on each side of the
+ *      base, another blank space, and the total number of cannonballs in the tetrahedron.
+ *
+ */
+
+public class Driver
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+
+    private static int loopedArea(int base)
+    {
+        return 0;
+    }
+
+    private static int mathArea(int base)
+    {
+        return 0;
+    }
+}
